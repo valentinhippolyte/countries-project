3038407cadc1b0afbc09c37670fd7ecbbcccebc5
Valentin Hippolyte <valenthippo@gmail.com>
2022-12-05 21:30:14 +0100

    clean code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,11 @@
+<script>
+// import Vue from 'vue'
+// import Vuesax from 'vuesax'
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <p>This is an About page</p>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
