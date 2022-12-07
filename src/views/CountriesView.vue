92927de33f91616225e35ea5491427ba5b456796
Valentin Hippolyte <valenthippo@gmail.com>
2022-12-07 21:32:16 +0100

    favorites cards

--- src/views/CountriesView.vue
+++ src/views/CountriesView.vue
@@ -1,7 +1,25 @@
-<script></script>
+<script setup>
+import {
+  // useFirestore,
+  getCurrentUser,
+  // useCollection,
+  useCurrentUser,
+} from "vuefire";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getFirestore,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+const user = useCurrentUser();
+</script>
 
 <template>
-  <v-text-field label="query" v-model="query" />
+  <v-text-field label="query" v-model="searchQuery" />
   <v-btn icon @click="search">
     <i class="material-icons">search</i>
   </v-btn>
@@ -10,24 +28,23 @@
   <div class="cards">
     <v-card
       :key="index"
-      v-for="(countries, index) in countries"
+      v-for="(country, index) in countries"
       class="card"
       max-width="400"
     >
       <v-img
         class="align-end text-white"
         height="200"
-        :src="countries?.flags?.png"
+        :src="country?.flags?.png"
         cover
       >
       </v-img>
       <div class="label">
-        {{ countries.name?.common }}
+        {{ country.name?.common }}
         <v-card-actions
-          ><v-btn color="orange-lighten-2" variant="text">
-            <i class="material-icons">favorite</i>
-          </v-btn></v-card-actions
-        >
+          ><v-btn v-if="user" :icon="country.isFavorite ? 'mdi-star-check' : 'mdi-star'" color="primary"
+            @click="toggleFavorite(country)">
+          </v-btn></v-card-actions>
       </div>
     </v-card>
   </div>
@@ -39,6 +56,7 @@ export default {
   data() {
     return {
       countries: [],
+      searchQuery: "",
     };
   },
   methods: {
@@ -50,14 +68,50 @@ export default {
     },
     async search() {
       const baseUrl = "https://restcountries.com/v3.1";
-      const url = `${baseUrl}/name/${this.query}`;
+      const url = `${baseUrl}/name/${this.searchQuery}`;
       const response = await fetch(url);
       const data = await response.json();
       this.countries = data;
     },
+    async isFavorite(country) {
+      const db = getFirestore();
+      const currentUser = await getCurrentUser();
+      const collectionName = `favorties-${currentUser.uid}`;
+      const q = query(
+        collection(db, collectionName),
+        where("name", "==", country.name.common)
+      );
+      const querySnapshot = await getDocs(q);
+      console.log("is Favorite: ", !querySnapshot.empty);
+      return !querySnapshot.empty;
+    },
+    async toggleFavorite(country) {
+      const db = getFirestore();
+      const currentUser = await getCurrentUser();
+      const collectionName = `favorties-${currentUser.uid}`;
+      if (!country.isFavorite) {
+        await addDoc(collection(db, collectionName), {
+          name: country.name.common,
+        });
+      } else {
+        const q = query(
+          collection(db, collectionName),
+          where("name", "==", country.name.common)
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach(async (document) => {
+          await deleteDoc(doc(db, collectionName, document.id));
+        });
+      }
+      country.isFavorite = !country.isFavorite;
+    },
   },
-  mounted() {
+  async mounted() {
     this.getAll();
+    for (const country of this.countries) {
+      country.isFavorite = await this.isFavorite(country);
+    }
+    console.table(this.countries);
   },
 };
 </script>
